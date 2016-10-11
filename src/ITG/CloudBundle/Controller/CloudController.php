e0460729bd463947f45e67ddb8190a17d7897c8e
rishabh dev bansal <rishabh.bansal@gaadi.com>
2016-10-11 13:21:44 +0530

    cloud fx rates api integration commit

--- src/ITG/CloudBundle/Controller/CloudController.php
+++ src/ITG/CloudBundle/Controller/CloudController.php
@@ -24,18 +24,50 @@ class CloudController extends BaseController
      public function postCloudloginAction(Request $request)
     {
 			$url = "https://devapi.thecurrencycloud.com/v2/authenticate/api";
-			$ch = curl_init();
+			$postArray = array('login_id' => 'talkremit.api', 'api_key' => 'cdbeb4766fa4da19214ef1455c75f97d6e954c69f9e4f1af161f9cb0a66c5257');
+             
+            $retunAuthVal = $this->initiateCrossDomainRequest($url, $postArray, 'POST', false, array());
+            $retunAuthArray = json_decode($retunAuthVal,true);
+             $auth_token = $retunAuthArray['auth_token'];
+             $headers = array("X-Auth-Token: $auth_token");
+             $fxPostArray  = array('buy_currency' => 'USD', 'sell_currency' =>'GBP', 'amount' => '100', 'fixed_side' => 'buy');
+             $fxAPIUrl = 'https://devapi.thecurrencycloud.com/v2/rates/detailed';
+             $retunFxVal = $this->initiateCrossDomainRequest($fxAPIUrl, $fxPostArray, 'GET', true, $headers);
+             echo $retunFxVal;
+             exit(); 
+    }
 
-			$options = array(
-			CURLOPT_CUSTOMREQUEST => "POST",
-			CURLOPT_POSTFIELDS => array('login_id' => 'talkremit.api', 'api_key' => 'cdbeb4766fa4da19214ef1455c75f97d6e954c69f9e4f1af161f9cb0a66c5257'), 
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_FOLLOWLOCATION => true,
-			CURLOPT_SSL_VERIFYHOST => false,
-			CURLOPT_SSL_VERIFYPEER => false,
-			//  CURLOPT_HTTPHEADER  => $headers,
-			CURLOPT_URL => $url
-			);
+
+    private function initiateCrossDomainRequest($url, $postArray, $method, $flag, $header)
+    {
+     
+            $ch = curl_init();
+            if(!$flag){
+					$options = array(
+					CURLOPT_CUSTOMREQUEST => $method,
+					CURLOPT_POSTFIELDS => $postArray, 
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_FOLLOWLOCATION => true,
+					CURLOPT_SSL_VERIFYHOST => false,
+					CURLOPT_SSL_VERIFYPEER => false,
+					CURLOPT_URL => $url
+					);
+            	
+            }
+            else{
+
+            	$options = array(
+					CURLOPT_CUSTOMREQUEST => $method,
+					CURLOPT_POSTFIELDS => $postArray, 
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_FOLLOWLOCATION => true,
+					CURLOPT_SSL_VERIFYHOST => false,
+					CURLOPT_SSL_VERIFYPEER => false,
+					CURLOPT_HTTPHEADER  => $header,
+					CURLOPT_URL => $url
+					);
+            }
+			
 
 			curl_setopt_array($ch , $options);
 
@@ -47,10 +79,9 @@ class CloudController extends BaseController
 			}
 			else
 			{
-			   echo htmlentities($output);
+			  return $output;
 			}
-    	exit();
-       
+    	
     }
 
 }
