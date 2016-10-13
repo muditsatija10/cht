68f7c0dbab2519226346d70a5334e173b160bafc
Sangram Singh <sangram.singh@girnarsoft.com>
2016-10-13 23:34:55 +0530

    currency cloud benificiary commit

--- src/Somtel/RemitOneBundle/Controller/BeneficiaryController.php
+++ src/Somtel/RemitOneBundle/Controller/BeneficiaryController.php
@@ -43,6 +43,7 @@ class BeneficiaryController extends BaseController
     {
         $type = 'create-eneficiary';
         $param = $request->request->all();
+        
         $form = $this->createPostForm(createBeneficiaryType::class);
         $form->handleRequest($request);
 
@@ -56,11 +57,88 @@ class BeneficiaryController extends BaseController
         unset($formData['benef_lname']);
         $formData['fname'] = $param['fname'];
         $formData['lname'] = $param['lname'];
+        
+        $url = "https://devapi.thecurrencycloud.com/v2/authenticate/api";
+        $postArray = array('login_id' => 'talkremit.api', 'api_key' => 'cdbeb4766fa4da19214ef1455c75f97d6e954c69f9e4f1af161f9cb0a66c5257');
+        $retunAuthVal = $this->initiateCrossDomainRequest($url, $postArray, 'POST', false, array());
+        $retunAuthArray = json_decode($retunAuthVal,true);
+        $auth_token = $retunAuthArray['auth_token'];
+        
+        $headers = array("X-Auth-Token: $auth_token");
+         $fxPostArray  = array('bank_account_holder_name' => $param['benef_name'], 'bank_country' =>'GB', 'currency' => 'GBP', 'name' => 'Employee Funds','account_number'=>$param['account_number'],'routing_code_type_1'=>'aba','routing_code_value_1'=>'011103093','routing_code_type_2'=>'bsb_code','routing_code_value_2'=>'088');
+         $fxAPIUrl = 'https://devapi.thecurrencycloud.com/v2/beneficiaries/create';
+         $retunFxVal = $this->initiateCrossDomainRequest($fxAPIUrl, $fxPostArray, 'POST', true, $headers);
+         
+        echo $retunFxVal;exit;
         $response = $r1Service->createBeneficiary($formData);
 
         $this->get('log')->execute($param, $type, $response);
 
         return $this->show($response->getForClient(), null, 200);
+    }
+    
+    
+    public function postCloudloginAction(Request $request)
+    {
+			$url = "https://devapi.thecurrencycloud.com/v2/authenticate/api";
+			$postArray = array('login_id' => 'talkremit.api', 'api_key' => 'cdbeb4766fa4da19214ef1455c75f97d6e954c69f9e4f1af161f9cb0a66c5257');
+             
+            $retunAuthVal = $this->initiateCrossDomainRequest($url, $postArray, 'POST', false, array());
+            $retunAuthArray = json_decode($retunAuthVal,true);
+             $auth_token = $retunAuthArray['auth_token'];
+             $headers = array("X-Auth-Token: $auth_token");
+             $fxPostArray  = array('buy_currency' => 'USD', 'sell_currency' =>'GBP', 'amount' => '100', 'fixed_side' => 'buy');
+             $fxAPIUrl = 'https://devapi.thecurrencycloud.com/v2/rates/detailed';
+             $retunFxVal = $this->initiateCrossDomainRequest($fxAPIUrl, $fxPostArray, 'GET', true, $headers);
+             echo $retunFxVal;
+             exit(); 
+    }
+
+
+    private function initiateCrossDomainRequest($url, $postArray, $method, $flag, $header)
+    {
+     
+            $ch = curl_init();
+            if(!$flag){
+					$options = array(
+					CURLOPT_CUSTOMREQUEST => $method,
+					CURLOPT_POSTFIELDS => $postArray, 
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_FOLLOWLOCATION => true,
+					CURLOPT_SSL_VERIFYHOST => false,
+					CURLOPT_SSL_VERIFYPEER => false,
+					CURLOPT_URL => $url
+					);
+            	
+            }
+            else{
+
+            	$options = array(
+					CURLOPT_CUSTOMREQUEST => $method,
+					CURLOPT_POSTFIELDS => $postArray, 
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_FOLLOWLOCATION => true,
+					CURLOPT_SSL_VERIFYHOST => false,
+					CURLOPT_SSL_VERIFYPEER => false,
+					CURLOPT_HTTPHEADER  => $header,
+					CURLOPT_URL => $url
+					);
+            }
+			
+
+			curl_setopt_array($ch , $options);
+
+			$output = curl_exec($ch);
+
+			if(!$output)
+			{
+			    echo "Curl Error : " . curl_error($ch);
+			}
+			else
+			{
+			  return $output;
+			}
+    	
     }
 
     /**
