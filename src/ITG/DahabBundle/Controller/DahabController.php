d0fb258c8beb1d0132230a2da231a383fe680bf8
rishabh dev bansal <rishabh.bansal@gaadi.com>
2016-10-15 19:00:06 +0530

    dahab api integration commit

--- src/ITG/DahabBundle/Controller/DahabController.php
+++ src/ITG/DahabBundle/Controller/DahabController.php
@@ -18,16 +18,17 @@ class DahabController extends BaseController
 
 	/**
 	 * @ApiDoc()
-     * @Post("/customer")
+     * @Post("/validateCustomer")
      */
 
-     public function postCustomerAction(Request $request)
+     public function postValidateCustomerAction(Request $request)
     {
     	  
-    	$postData = $request->getContent();
-        $postDataArray  = json_decode($postData);
-
-        $url = "https://www.dahabonline.co.uk/talkRemit/Validate/Customer?HASH=0ef0c5a96950d1e8ce0d0f173c152291bb722778cbddb6efe7d9b86b5e0acaae&API_KEY=21f3ab93-b5f9-4118-968c-6a45f551e1e6";
+    	 $postData = $request->getContent();
+         $apiKey = '222be339-9c09-45d6-be77-03c8b3e0d7c9';
+         $hashString = $postData.$apiKey;
+         $hash = hash("sha256", $hashString);
+        $url = "https://www.dahabonline.co.uk/talkRemit/Validate/Customer?HASH=".$hash."&API_KEY=21f3ab93-b5f9-4118-968c-6a45f551e1e6";
 		$cert_file = '/var/www/html/somtel/file.crt.pem';
 		$cert_key = '/var/www/html/somtel/file.key.pem';
 		$cert_password = '#$SGADS9XGH927T&$%$';
@@ -64,6 +65,111 @@ class DahabController extends BaseController
 		}
         exit();
        
+    }
+
+
+    /**
+	 * @ApiDoc()
+     * @Post("/processPayment")
+     */
+
+     public function postProcessPaymentAction(Request $request)
+    {
+
+         $postData = $request->getContent();
+         $apiSecret = '222be339-9c09-45d6-be77-03c8b3e0d7c9';
+         $hashString = $postData.$apiSecret;
+         $hash = hash("sha256", $hashString);
+         $url = "https://www.dahabonline.co.uk/talkRemit/Process/Payment?HASH=".$hash."&API_KEY=21f3ab93-b5f9-4118-968c-6a45f551e1e6";
+		$cert_file = '/var/www/html/somtel/file.crt.pem';
+		$cert_key = '/var/www/html/somtel/file.key.pem';
+		$cert_password = '#$SGADS9XGH927T&$%$';
+		$headers = array('Content-Type: application/json');
+		$data_string = $postData; 
+		$ch = curl_init();
+		 
+		$options = array(
+		    CURLOPT_CUSTOMREQUEST => "POST",
+		    CURLOPT_POSTFIELDS => $data_string, 
+		    CURLOPT_RETURNTRANSFER => true,
+		    CURLOPT_FOLLOWLOCATION => true,
+		    CURLOPT_SSL_VERIFYHOST => false,
+		    CURLOPT_SSL_VERIFYPEER => false,
+		    CURLOPT_HTTPHEADER  => $headers,
+		    CURLOPT_URL => $url ,
+		    CURLOPT_SSLCERT => $cert_file ,
+		    CURLOPT_SSLKEY => $cert_key,
+		    CURLOPT_SSLCERTPASSWD => $cert_password,
+		    CURLOPT_SSLKEYPASSWD => $cert_password
+		);
+		 
+		curl_setopt_array($ch , $options);
+		 
+		$output = curl_exec($ch);
+		 
+		if(!$output)
+		{
+		    echo "Curl Error : " . curl_error($ch);
+		}
+		else
+		{
+		    echo $output;
+		}
+        exit();
+
+
+    }
+
+
+     /**
+	 * @ApiDoc()
+     * @Post("/sendSMS")
+     */
+
+     public function postSendSMSAction(Request $request)
+    {
+
+         $postData = $request->getContent();
+         $apiSecret = '222be339-9c09-45d6-be77-03c8b3e0d7c9';
+         $hashString = $postData.$apiSecret;
+         $hash = hash("sha256", $hashString);
+         $url = "https://www.dahabonline.co.uk/talkRemit/Send/SMS?HASH=".$hash."&API_KEY=21f3ab93-b5f9-4118-968c-6a45f551e1e6";
+		$cert_file = '/var/www/html/somtel/file.crt.pem';
+		$cert_key = '/var/www/html/somtel/file.key.pem';
+		$cert_password = '#$SGADS9XGH927T&$%$';
+		$headers = array('Content-Type: application/json');
+		$data_string = $postData; 
+		$ch = curl_init();
+		 
+		$options = array(
+		    CURLOPT_CUSTOMREQUEST => "POST",
+		    CURLOPT_POSTFIELDS => $data_string, 
+		    CURLOPT_RETURNTRANSFER => true,
+		    CURLOPT_FOLLOWLOCATION => true,
+		    CURLOPT_SSL_VERIFYHOST => false,
+		    CURLOPT_SSL_VERIFYPEER => false,
+		    CURLOPT_HTTPHEADER  => $headers,
+		    CURLOPT_URL => $url ,
+		    CURLOPT_SSLCERT => $cert_file ,
+		    CURLOPT_SSLKEY => $cert_key,
+		    CURLOPT_SSLCERTPASSWD => $cert_password,
+		    CURLOPT_SSLKEYPASSWD => $cert_password
+		);
+		 
+		curl_setopt_array($ch , $options);
+		 
+		$output = curl_exec($ch);
+		 
+		if(!$output)
+		{
+		    echo "Curl Error : " . curl_error($ch);
+		}
+		else
+		{
+		    echo $output;
+		}
+        exit();
+
     }
 
 }
