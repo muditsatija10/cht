bdfb80f726488bac5a839998d49548e72d773e39
rishabh dev bansal <rishabh.bansal@gaadi.com>
2016-10-15 11:42:47 +0530

    conversion api commit

--- src/ITG/CloudBundle/Controller/CloudController.php
+++ src/ITG/CloudBundle/Controller/CloudController.php
@@ -83,7 +83,8 @@ class CloudController extends BaseController
 			}
     	
     }
-    	/**
+
+    /**
 	 * @ApiDoc()
      * @Post("/validateBenificiary")
      */
@@ -146,6 +147,24 @@ class CloudController extends BaseController
         }
         
        return json_encode($dataToSend);
+    }
+
+
+    /**
+	 * @ApiDoc()
+     * @Post("/createConversion")
+     */
+
+     public function postCreateConversionAction(Request $request)
+    {
+            $url = "https://devapi.thecurrencycloud.com/v2/authenticate/api";
+			$postArray = array('login_id' => 'talkremit.api', 'api_key' => 'cdbeb4766fa4da19214ef1455c75f97d6e954c69f9e4f1af161f9cb0a66c5257');
+             
+            $retunAuthVal = $this->initiateCrossDomainRequest($url, $postArray, 'POST', false, array());
+            $retunAuthArray = json_decode($retunAuthVal,true);
+            $auth_token = $retunAuthArray['auth_token'];
+            $headers = array("X-Auth-Token: $auth_token");
+            
     }
 
 }
